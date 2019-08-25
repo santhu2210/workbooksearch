c471652dc38784fc73b24768aa91dbcc9df69053
santh <p.shanthakumar@spi-global.com>
2019-08-25 09:48:42 +0530

    changes in nan removal, combined multisheet, remove truncated string

--- app.py
+++ app.py
@@ -16,7 +16,10 @@ DATA_DIR = 'data'
 XL_FILE = 'sbc_data.xlsx'
 CARRIER_COLUMN_NAME = 'Carrier_Name'
 TG_COLUMN_NAME = 'TG_Name'
+CARRIER_IP_COLUMN_NAME='Carrier_IP'
 STATUS_COLUMN_NAME = 'Status'
+OUTPUT_COLUMNS = ['Carrier_Name','Switch','TG_Name','Direction','Nop ID','Bharti GW IP','Carrier_IP','Capacity (Circuits)','Status']
+
 
 ## Initialize flask app 
 app = Flask(__name__)
@@ -31,28 +34,44 @@ def main():
     return render_template('index.html')
 
 
+
 ## Search call rounting function
 @app.route('/search', methods=['POST'])
 def search():
     # read the posted values from the UI
     keyword = request.form['inputKeyword']
-    test_file_path = os.path.join(BASE_DIR, DATA_DIR, XL_FILE)
 
-    sheet_to_df_dict = read_xls(test_file_path)
     InfoDF = pd.DataFrame()
 
     for sht, df in sheet_to_df_dict.items():
-        car_df = df[(df[CARRIER_COLUMN_NAME].str.contains(str(keyword), case=False)) & (df[STATUS_COLUMN_NAME] == 'LIVE')]
-        tg_df = df[(df[TG_COLUMN_NAME].str.contains(str(keyword), case=False)) & (df[STATUS_COLUMN_NAME] == 'LIVE')]
-        tempDF = pd.concat([car_df, tg_df])
+        try:
+            # print("sht name :", sht)
+            car_df = df[(df[CARRIER_COLUMN_NAME].str.contains(str(keyword),na=False, case=False)) & (df[STATUS_COLUMN_NAME] == 'LIVE')]
+            tg_df = df[(df[TG_COLUMN_NAME].str.contains(str(keyword),na=False, case=False)) & (df[STATUS_COLUMN_NAME] == 'LIVE')]
+            ip_df = df[(df[CARRIER_IP_COLUMN_NAME].str.contains(str(keyword),na=False, case=False)) & (df[STATUS_COLUMN_NAME] == 'LIVE')]
+            tempDF = pd.DataFrame()
+            tempDF = pd.concat([car_df, tg_df, ip_df])
 
-        InfoDF = pd.concat([InfoDF,tempDF])
+            if len(tempDF) > 0:
+                InfoDF= InfoDF.append(tempDF, ignore_index = True) 
+                #InfoDF = pd.concat([InfoDF,tempDF])
 
-    req_df = InfoDF.iloc[:,0:9]
-    #req_df.reset_index(drop=True, inplace=True)
+        except:
+            pass
+
+    # print('InfoDF',InfoDF.OUTPUT_COLUMNS)
+    pd.set_option('display.max_colwidth', -1)
+    pd.options.display.float_format = '{:,.0f}'.format
+
+    if len(InfoDF) > 0:
+        req_df = InfoDF[OUTPUT_COLUMNS]#.iloc[:,0:9]
+        # df.a = df.a.astype(float)
+    else:
+        req_df = pd.DataFrame()
+
     tables_html=[req_df.to_html(classes="table table-striped table-bordered search-data ", index=False, header="true")]
 
-    #return json.dumps(req_df.to_dict('records'))
+    # print(json.dumps(req_df.to_dict('records')))
 
     if len(tables_html[0]) > 450 :
         search_result = remove_escape_characters(tables_html[0])
@@ -86,7 +105,10 @@ def read_xls(test_file):
     return sheet_to_df_dict
 
 
+test_file_path = os.path.join(BASE_DIR, DATA_DIR, XL_FILE)
+sheet_to_df_dict = read_xls(test_file_path)
+
 ## main function
 if __name__ == "__main__":
-    app.run(debug=True, host= '0.0.0.0', port=5555)
+    app.run(debug=True, host= '0.0.0.0', port=5556)
 
